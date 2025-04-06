40583ac446155add4d8f4c84745bff6f352fb80e
Юрий Кишко <yurykishko@yandex.ru>
2025-04-06 14:20:48 +0800

    без настроек pinia

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,9 @@
       <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo">
       <h1>Pinia</h1>
     </header>
-
-      <h1>Count is {{ store.count }}</h1>
-      <h2>Double is {{ store.doubleCount }}</h2>
-      <button @click="store.increment (1)">Increment</button>
-      <button @click="store.waitAndAdd">Wait + Increment</button>
   </main>
 </template>
 
 <script setup>
-import {storeToRefs } from 'pinia'
-import { useCounterStore } from "./store/counter";
-const store = useCounterStore();
-const { count } = storeToRefs(store) // REACTIVE
-console.log(count) // => REF
-store.count++;
-store.increment(2);
+
 </script>
